7be4fbf307ece7d4f2a4844a364af1cd39e33e28
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-23 16:03:59 -0400

    Move async function to /api directory

---
+++ src/api/fetchShow.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const fetchShow = () => {
+    return axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(res => {
+        console.log(res.data);
+        return res.data;
+      })
+      .catch(error => {
+          return error;
+      });
+  };
